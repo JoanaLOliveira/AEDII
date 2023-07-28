703ceb7d30e79733fb22d203bc0fcf6e1b12303a
Rafael O. Freire <120592511+rfreir3@users.noreply.github.com>
2023-07-28 16:38:39 +0000

    tempo de execução bubble sort

---
+++ Primeira Unidade/algorithm_time.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+
+void bubbleSort(int *vetor, int tamanho) {
+    int i, j, aux;
+    for (i = 0; i < tamanho; i++) {
+        for (j = 0; j < tamanho - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+            }
+        }
+    }
+}
+
+int main() {
+    int vetor[] = {5, 3, 2, 4, 7, 1, 0, 6};
+    int tamanho = sizeof(vetor) / sizeof(int);
+    int i;
+    clock_t start;
+    bubbleSort(vetor, tamanho);
+    clock_t end;
+    for (i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\nTempo de execução bubble sort: %li", (start - end)/(CLOCKS_PER_SEC));
+    return 0;
+}
