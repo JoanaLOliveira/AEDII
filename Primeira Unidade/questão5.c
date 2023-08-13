1139626cf20db0a80a02c1bcb4b502457799e087
Rafael O. Freire <120592511+rfreir3@users.noreply.github.com>
2023-08-13 12:36:29 -0300

    Update and rename produto_recursivo.c to questão5.c

--- Primeira Unidade/questão5.c
+++ Primeira Unidade/questão5.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int produto(int x1, int x2) {
+int produto(int x1, int x2){
     if (x2 == 0) {
-        return 0;
+        return (0);
     }
     else if(x2 == 1){
-        return x1;
+        return (x1);
     }
-    return x1 + produto(x1, x2 - 1);
+    return (x1 + produto(x1, x2 - 1));
 }
 
-int main() {
+int main(){
     int x1, x2;
     printf("Digite o valor de x1: ");
     scanf("%d", &x1);
@@ -18,5 +18,5 @@ int main() {
     scanf("%d", &x2);
     int y = produto(x1, x2);
     printf("O produto de %d e %d e %d\n", x1, x2, y);
-    return 0;
+    return (0);
 }
